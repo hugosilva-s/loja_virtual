6f9f1a4e4dfe19b2213d6f340d1e61830ad6d75b
H <hugosilva905@gmail.com>
2022-04-22 20:00:34 -0300

    commit classe cat produto

--- src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package jdev.mentoria.lojavirtual.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,14 +15,13 @@ import javax.persistence.Table;
 @SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto implements Serializable {
 
-
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nomeDesc;
 	
 	public Long getId() {
@@ -35,5 +35,30 @@ public class MarcaProduto implements Serializable {
 	
 	public String getNomeDesc() {
 		return nomeDesc;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarcaProduto other = (MarcaProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
