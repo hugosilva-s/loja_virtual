ecb71bb3807377a626bcab4f63b52e89cc1721f9
H <hgo737@hotmail.com>
2022-06-02 21:40:11 -0300

    End-point junit

--- src/test/java/com/example/demo/LojaVirtualApplicationTests.java
+++ src/test/java/com/example/demo/LojaVirtualApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +19,9 @@ public class LojaVirtualApplicationTests {
 	
 	@Autowired
 	private AcessoService acessoService;
-	//@Autowired
-	//private AcessoRepository acessoRepository;
+	
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 	@Autowired
 	private AcessoController acessoController;
@@ -27,7 +32,43 @@ public class LojaVirtualApplicationTests {
 		Acesso acesso = new Acesso();
 		acesso.setDescricao("ROLE_ADMIN");
 		
-		acessoController.salvarAcesso(acesso);
+		assertEquals(true, acesso.getId() == null);
+		
+		/*Gravou aceeaso no banco de dados*/
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+		
+		/*Teste de carregamento*/
+		
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+		assertEquals(acesso.getId(), acesso2.getId());
+		
+		/*Teste de delete*/
+		
+		acessoRepository.deleteById(acesso2.getId());
+		
+		acessoRepository.flush(); /*Roda esse sql de delete no banco*/
+		
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+		assertEquals(true, acesso3 == null);
+		
+		/*Teste query*/
+		
+		acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ALUNO");
+		
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+		
+		assertEquals(1, acessos.size());
+		
+		acessoRepository.deleteById(acesso.getId());
 	}
 
 }
